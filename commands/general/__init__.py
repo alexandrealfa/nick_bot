079882a033c1a38d8a9a9e64ec195a9e379eeaad
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-09-24 14:40:23 -0400

    (master): - Implements help command, and docummentation about commands.

--- commands/general/__init__.py
+++ commands/general/__init__.py
@@ -3,12 +3,15 @@ from typing import NoReturn
 import discord
 from discord.ext import commands
 
-from config import BAD_WORDS_FILENAME
-from helper import get_words
+from config import BAD_WORDS_FILENAME, COMMANDS_FILENAME
+from helpers.csv_helper import get_words, get_all_rows
+from helpers.embed import EmbedHelper
 
 
 @commands.command()
 async def hi(ctx: discord) -> NoReturn:
+    """Diga ola ao nick."""
+
     if name := ctx.author.name:
         await ctx.send(f'ola, {name}')
 
@@ -17,6 +20,7 @@ async def hi(ctx: discord) -> NoReturn:
 
 @commands.command()
 async def ping(ctx: discord, message=None) -> NoReturn:
+    """Verifica se o bot está ativo."""
     if not message or len(message) == 0:
         await ctx.send('test')
 
@@ -29,23 +33,57 @@ async def ping(ctx: discord, message=None) -> NoReturn:
 
 @commands.command()
 async def repository(ctx: discord) -> NoReturn:
-    await ctx.send('```NickBot Repository:``` https://github.com/alexandrealfa/nick_bot')
+    """Mostra o repositorio do nickbot no Github."""
+    author = {
+        'name': 'Alexandre Alfa',
+        'icon_url': 'https://avatars.githubusercontent.com/u/46616348?v=4',
+        'url': 'https://github.com/alexandrealfa'
+    }
+
+    ebd = EmbedHelper(
+        "Repositorio do Nickbot.",
+        discord.Color.green(),
+        url='https://github.com/alexandrealfa/nick_bot',
+        author=author,
+        thumb='https://opengraph.githubassets.com/8014b1adfaa7268a0a95a15d163e795ba03a031d4de95932a900cd979f324977/alexandrealfa/nick_bot'
+    )
+    ebd.generate_embed()
+
+    await ctx.send(embed=ebd.embed)
 
     return
 
 
 @commands.command()
-async def show_bad_words(ctx: discord, message=None) -> NoReturn:
+async def help_commands(ctx: discord) -> NoReturn:
+    """Mostra todos os Commands do nick bot"""
+
+    if all_commands := get_all_rows(COMMANDS_FILENAME):
+        list_of_fields = [{'name': row[0], 'value': row[1], 'inline': False} for row in all_commands]
+
+        ebd = EmbedHelper("Lista de Comandos.", discord.Color.green(), fields=list_of_fields)
+        ebd.generate_embed()
+
+        await ctx.send(embed=ebd.embed)
+
+        return
+
+    await ctx.send('A lista de commandos, está vazia.')
+
+    return
+
+
+@commands.command()
+async def show_bad_words(ctx: discord) -> NoReturn:
+    """Mostra a lista de palavras bloqueadas."""
 
     if bad_words_list := get_words(BAD_WORDS_FILENAME):
-        embed = discord.Embed(
-            title="Palavras Bloqueadas.",
-            color=discord.Color.green()
-        )
+        list_of_fields = [{'name': name, 'value': 'Bloqueado', 'inline': False} for name in bad_words_list]
 
-        [embed.add_field(name=name, value='Bloqueada', inline=False) for name in bad_words_list]
+        ebd = EmbedHelper("Palavras Bloqueadas.", discord.Color.green(), fields=list_of_fields)
+        ebd.generate_embed()
 
-        await ctx.send(embed=embed)
+        await ctx.send(embed=ebd.embed)
 
         return
 
@@ -56,4 +94,5 @@ async def show_bad_words(ctx: discord, message=None) -> NoReturn:
 
 @commands.command()
 async def append_bad_word(ctx: discord, message=None) -> NoReturn:
+    """Adiciona a palavra a lista de bloqueio"""
     pass
