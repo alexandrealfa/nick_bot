bafb9016c8f3f8e8326e06c8aec30256e7bb8700
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-09-24 14:38:15 -0400

    (master): - Implements embed helper

---
+++ helpers/csv_helper.py
@@ -0,0 +1,38 @@
+import csv
+from typing import NoReturn, Tuple, Any
+
+import pandas as pd
+
+
+def get_words(filename: str, key: str = 'bad_words') -> list:
+    data = pd.read_csv(filename)
+
+    return list(data[key].to_list())
+
+
+def get_all_rows(filename: str) -> Any:
+    data = pd.read_csv(filename)
+
+    return data.to_numpy().tolist()
+
+
+def validate_word(filename: str, current_word: str) -> Tuple[bool, str]:
+    words, file_words = current_word.split(' '), get_words(filename)
+
+    if [v for v in file_words if v in current_word]:
+        return True, 'Você digitou uma palavra proibida nesse canal!.'
+
+    for word in words:
+        if word in file_words:
+            return True, 'O texto que você digitou contém uma palavra proibida nesse canal!.'
+
+    return False, ''
+
+
+def insert_word(filename: str, header: str, word: str) -> NoReturn:
+    all_words = get_words(filename)
+
+    if str(word) not in all_words:
+        all_words.append(str(word))
+        new_df = pd.DataFrame({header: all_words})
+        new_df.to_csv(filename)
---
+++ helpers/embed.py
@@ -0,0 +1,47 @@
+from discord import Embed as Ebd
+
+from typing import Dict, NoReturn, Union
+
+
+class EmbedHelper:
+    def __init__(
+            self,
+            title: str,
+            color: str,
+            description: Union[str, None] = None,
+            url: Union[str, None] = None,
+            thumb: Union[str, None] = None,
+            fields: Union[list[Dict], None] = None,
+            author: Union[Dict, None] = None
+    ):
+        self.title: str = title
+        self.color: str = color
+        self.description: Union[str, None] = description
+        self.url: Union[str, None] = url
+        self.thumb: Union[str, None] = thumb
+        self.fields: Union[list[Dict], None] = fields
+        self.embed: Union[Ebd, None] = None
+        self.author: Union[Dict, None] = author
+
+    def generate_embed(self):
+        self.embed = Ebd(title=self.title, color=self.color)
+
+        if self.description:
+            self.embed.description = self.description
+
+        if self.url:
+            self.embed.url = self.url
+
+        if self.thumb:
+            self.embed.set_thumbnail(url=self.thumb)
+
+        if self.author:
+            self.embed.set_author(**self.author)
+
+        if self.fields:
+            self.generate_fields()
+
+        return
+
+    def generate_fields(self) -> NoReturn:
+        [self.embed.add_field(**field) for field in self.fields]
